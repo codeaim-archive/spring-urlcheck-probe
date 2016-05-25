4f493efdce5bf4d10ba14e5625aadfd9145f508c
Gerard Downes <gdownes@gmail.com>
2016-05-26 00:18:02 +0100

    Removed unneeded autowired repo Added circle.yml for circle ci build

--- src/main/java/com/codeaim/urlcheck/Application.java
+++ src/main/java/com/codeaim/urlcheck/Application.java
@@ -8,9 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application
 {
-    @Autowired
-    UserRepository userRepository;
-
     public static void main(String[] args)
     {
         SpringApplication.run(Application.class, args);
