0044b9b8f7b7b9129e52d74bbae3f6b611e917e9
Gerard Downes <gdownes@gmail.com>
2016-07-08 09:03:35 +0100

    Move to using okHttpClient over RestTemplate Added async request of check urls

--- src/main/java/com/codeaim/urlcheck/domain/CheckDto.java
+++ src/main/java/com/codeaim/urlcheck/domain/CheckDto.java
@@ -3,7 +3,6 @@ package com.codeaim.urlcheck.domain;
 import okhttp3.HttpUrl;
 import org.apache.commons.lang3.Validate;
 
-import java.net.URL;
 import java.time.Instant;
 import java.util.Optional;
 import java.util.OptionalLong;
